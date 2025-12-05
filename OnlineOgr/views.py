03336bd9213ef31eb614b42565f4cbee970afcce
Muhammed Karagülle <161960723+Muhammedkaragulle@users.noreply.github.com>
2025-12-05 09:26:37 +0300

    Add new view functions for ulas and hakkimiz

--- OnlineOgr/views.py
+++ OnlineOgr/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'anasayfa.html')
+
+
+def ulas(request):
+    return render(request, 'bizeulasin.html')    
+
+def hakkimiz(request):
+    return render(request, 'hakkimizda.html')    
